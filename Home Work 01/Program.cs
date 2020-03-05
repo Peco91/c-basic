982ae130d1842c5dce1a4dd17c694a5602be29dc
Peco91 <58815251+Peco91@users.noreply.github.com>
2020-03-05 23:20:56 +0100

    homework1

---
+++ Home Work 01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Home_Work_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;            
+                
+                Console.WriteLine("STOP");
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
