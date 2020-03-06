8a630d1a3cbe736b44333787037e51d328df41f4
Peco91 <58815251+Peco91@users.noreply.github.com>
2020-03-06 23:58:33 +0100

    home work2
    exercise from class 02

---
+++ home work 05 06 07 exercise/home work 05 06 07/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace home_work_05_06_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercise 5
+
+            //int apples = 8;
+            //int basket = 5;
+
+            //Console.WriteLine("Enter numbers of threes");
+            //string input = Console.ReadLine();
+
+            //int threes = int.Parse(input);
+            //int totalApples = apples * 12 * threes;
+
+            //int result = 0;
+
+            //if (totalApples / basket == 0)
+            //{
+            //    result = totalApples / basket;
+            //}
+            //else
+            //{
+            //    result = (totalApples / basket) + 1;
+            //}
+            //Console.WriteLine(" you will nead " + result + " basket " + totalApples + "apples");
+
+            //Console.ReadLine();
+
+
+
+            //<==================================================================>
+
+
+
+
+
+            //exercise 6
+
+            //Console.WriteLine(" Please enter first number ");
+            //int firstNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(" Please enter second number ");
+            //int secondNumber = int.Parse(Console.ReadLine());
+
+            //int biger;
+            //if (firstNumber > secondNumber)
+            //{
+            //    biger = firstNumber;
+            //    Console.WriteLine("First number is biger");
+
+            //}else if (secondNumber > firstNumber)
+
+            //{
+            //    biger = secondNumber;
+            //    Console.WriteLine("Second number is biger");
+
+            //}else
+            //{
+            //    biger = firstNumber;
+            //    Console.WriteLine("Equal numbers");
+            //}
+
+            //if (biger != 0)
+            //{
+            //    if (biger % 2 == 0)
+            //    {
+            //        Console.WriteLine("This number is even");
+            //    }
+            //    else if (biger % 2 != 0)
+            //    {
+            //        Console.WriteLine("This number is odd");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(" Zero is an even number. In other words, its parity—the quality of an integer being even or odd—is even  it is an integer multiple of 2, specifically 0 × 2");
+            //    }
+            //}
+
+
+
+            //Console.ReadLine();
+
+
+
+
+            //<==============================================================================>
+
+
+
+
+
+            //Exercise 7 
+
+            //Console.WriteLine("Enter number from 1 to 3 and see whot you get :D");
+            //int input = int.Parse(Console.ReadLine());
+
+            //switch (input)
+            //{
+            //    case 1:
+            //Console.WriteLine("You got a new car ");
+            //break;
+
+            //    case 2:
+            //        Console.WriteLine("You got a new plane");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("You got a new bike");
+            //        break;
+            //}
+
+            //Console.ReadLine();
+        }
+    }
+}
