ddc294f0a50550c4e88fbd493a6173a66a87939f
Peco91 <58815251+Peco91@users.noreply.github.com>
2020-03-09 01:01:54 +0100

    home work 3
    dopolnitelni domasni

---
+++ Home Work 1-2-3/Home Work/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Home_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Home_Work calculator task1
+
+
+
+            //double number1;
+            //double number2;
+            //double sum;
+            //char signplusminusdividemultiplay = '+';
+
+            //Console.WriteLine("Pleas enter first number: ");
+            //number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(" Enter + - / * ");
+            //signplusminusdividemultiplay = Convert.ToChar(Console.ReadLine());
+
+            //Console.WriteLine("Please enter second number");
+            //number2 = Convert.ToDouble(Console.ReadLine());
+
+            //if (signplusminusdividemultiplay == '+')
+            //{
+            //    sum = number1 + number2;
+            //    Console.WriteLine("Your sum is " + sum);
+            //}
+            //else if (signplusminusdividemultiplay == '-')
+            //{
+            //    sum = number1 - number2;
+            //    Console.WriteLine("Your sum is " + sum);
+            //}
+            //else if (signplusminusdividemultiplay == '*')
+            //{
+            //    sum = number1 * number2;
+            //    Console.WriteLine("Your sum is " + sum);
+            //}
+            //else if (signplusminusdividemultiplay == '/')
+            //{
+            //    sum = number1 / number2;
+            //    Console.WriteLine("Your sum is " + sum);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Its working onli with numbers :D pls enter number");
+            //}
+            //Console.ReadLine();
+
+
+            //<================================================================================================>
+
+            // Home work 2 task2 Average number
+
+
+            //int ten, fifteen, twenty, thirty;
+
+            //Console.WriteLine("Pls enter 10");
+            //ten = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Pls enter 15");
+            //fifteen = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Pls enter 20");
+            //twenty = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Pls enter 30");
+            //thirty = int.Parse(Console.ReadLine());
+
+            //int avg = (ten + fifteen + twenty + thirty) / 4;
+            //Console.WriteLine("The average is : "  +  avg);
+            //Console.ReadLine();
+
+           // <=========================================================================>
+
+
+            // Home work 3   task 3 
+
+            //int num1, num2, swap;
+
+            //Console.WriteLine("Enter first number");
+            //num1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter second number");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Numbers below");
+            //Console.WriteLine("First number: " + num1);
+            //Console.WriteLine("Second number: " + num2);
+
+            //swap = num1;
+            //num1 = num2;
+            //num2 = swap;
+            //Console.WriteLine("Swap");
+            //Console.WriteLine("First number : " + num1);
+            //Console.WriteLine("Second number : " + num2);
+
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
